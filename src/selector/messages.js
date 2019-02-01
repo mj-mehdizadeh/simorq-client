2accc5f205e3e4370ba46f9154a999e7d147bc46
Mohammad Javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2019-02-01 14:26:13 +0330

    Typo fix rooms and messages selector

--- src/selector/messages.js
+++ src/selector/messages.js
@@ -1,13 +1,20 @@
 import {createSelector} from 'reselect';
-import {filter, map, findLast} from 'lodash';
 import {USER_ID} from '../constant/user';
+import {getRoomProp} from './rooms';
+import {filter, findLast, map, get} from 'lodash';
 
 export const getChatId = (state, props) => props.chatId;
 export const getMessages = state => state.messages;
 
 export const getMessage = (state, props) => state.messages[props.id];
-export const getMessageProps = (state, props) => state.messages[props.id] ? state.messages[props.id][props.key] : null;
-export const getRoomLastMessage = (state, props) => findLast(state.messages, {chatId: props.chatId});
+export const getMessageProp = (state, props) => state.messages[props.id] ? get(state.messages[props.id], props.key) : null;
+export const getRoomLastMessage = (state, props) =>
+  findLast(
+    state.messages,
+    {
+      chatId: props.chatId || getRoomProp(state, {roomId: props.roomId, key: 'subscribe.chatId'}),
+    },
+  );
 
 export const getRoomMessages = createSelector(
   getMessages,
--- src/selector/rooms.js
+++ src/selector/rooms.js
@@ -1,9 +1,9 @@
 import {createSelector} from 'reselect';
-import {filter, map} from 'lodash';
+import {filter, map, get} from 'lodash';
 
 export const getRooms = state => state.rooms;
 export const getRoom = (state, props) => state.rooms[props.roomId];
-export const getRoomProp = (state, props) => state.rooms[props.roomId] ? state.rooms[props.roomId][props.key] : null;
+export const getRoomProp = (state, props) => state.rooms[props.roomId] ? get(state.rooms[props.roomId], props.key) : null;
 
 export const getRoomList = createSelector(
   getRooms,
