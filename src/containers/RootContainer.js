c385a6b3b563128e48e3d529f180aabdcc6cfe56
Mohammad Javad Mehdizadeh <mj.mehdizadeh93@gmail.com>
2019-02-07 22:16:55 +0330

    redux persistStore

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Root, StyleProvider} from 'native-base';
 import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import {I18nextProvider} from 'react-i18next';
 import getTheme from '../theme/components';
 import variables from '../theme/variables/material';
@@ -10,7 +11,7 @@ import AppNavigator from '../navigation/AppNavigator';
 import {setContainer} from '../services/navigator';
 
 // create our store
-const store = createStore();
+const {store, persistor} = createStore();
 
 export default class RootContainer extends Component {
   setNavigationRef = (navigatorRef) => {
@@ -21,9 +22,11 @@ export default class RootContainer extends Component {
       <I18nextProvider i18n={i18n}>
         <StyleProvider style={getTheme(variables)}>
           <Provider store={store}>
-            <Root>
-              <AppNavigator ref={this.setNavigationRef}/>
-            </Root>
+            <PersistGate loading={null} persistor={persistor}>
+              <Root>
+                <AppNavigator ref={this.setNavigationRef}/>
+              </Root>
+            </PersistGate>
           </Provider>
         </StyleProvider>
       </I18nextProvider>
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,16 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import rootSaga from '../sagas';
 
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 // creates the store
 export default function configureStore() {
   const middleware = [];
@@ -19,9 +29,12 @@ export default function configureStore() {
   enhancers.push(applyMiddleware(...middleware));
 
   /* ------------- Redux Configuration ------------- */
-  const store = createStore(reducer, compose(...enhancers));
+  const store = createStore(persistedReducer, compose(...enhancers));
   // kick off root saga
   sagaMiddleware.run(rootSaga);
 
-  return store;
+  /* ------------- Persist Redux Configuration ------------- */
+  let persistor = persistStore(store);
+
+  return {store, persistor};
 }
